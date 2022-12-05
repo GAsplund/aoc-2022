6f962898e6dfee07bb767f37540d5f2296f77bad
GAsplund <gasplund2@gmail.com>
2022-12-05 22:37:02 +0100

    Cleanup and optimisation

--- Day 04 - Prolog/solution.pl
+++ Day 04 - Prolog/solution.pl
@@ -1,11 +1,9 @@
 start:-
     get_input(Input),
 
-    solve_s1(Input, S1solution, 0),
+    solve(Input, S1solution, S2solution, 0, 0),
     write('Solution 1: '),
     writeln(S1solution),
-
-    solve_s2(Input, S2solution, 0),
     write('Solution 2: '),
     writeln(S2solution).
 
@@ -27,10 +25,7 @@ get_input(Stream, Line, Acc, Input):-
 
 is_empty(List):- not(member(_,List)).
 
-in_range(N, R1, R2, Return):-
-    Return is (N >= R1) ; (R2 >= N).
-
-range_inside([R1, R2]):- 
+range_numbers(R1, R2, R1a, R1b, R2a, R2b):-
     % Split range into values
     split_string(R1, '-', '', [R1a_s, R1b_s]),
     split_string(R2, '-', '', [R2a_s, R2b_s]),
@@ -38,33 +33,29 @@ range_inside([R1, R2]):-
     atom_number(R1a_s, R1a),
     atom_number(R1b_s, R1b),
     atom_number(R2a_s, R2a),
-    atom_number(R2b_s, R2b),
+    atom_number(R2b_s, R2b).
+
+range_inside([R1, R2]):- 
+    range_numbers(R1, R2, R1a, R1b, R2a, R2b),
     % Check for containment
     (((R1a >= R2a) , (R1b =< R2b)) ; ((R2a >= R1a) , (R2b =< R1b))).
 
 range_overlap([R1, R2]):- 
-    % Split range into values
-    split_string(R1, '-', '', [R1a_s, R1b_s]),
-    split_string(R2, '-', '', [R2a_s, R2b_s]),
-    % Parse range values to integers
-    atom_number(R1a_s, R1a),
-    atom_number(R1b_s, R1b),
-    atom_number(R2a_s, R2a),
-    atom_number(R2b_s, R2b),
+    range_numbers(R1, R2, R1a, R1b, R2a, R2b),
     % Check for overlap
     (R1a =< R2b), (R1b >= R2a).
 
 incr(X, X1) :-
     X1 is X+1.
 
-solve_s1([Current|Next], InsideRanges, Count):-
+solve([Current|Next], InsideRanges, OverlapRanges, CountInside, CountOverlap):-
     string_chars(Ranges, Current),
     split_string(Ranges, ',', '', Intervals),
-    ((range_inside(Intervals) -> incr(Count, NewCount)); NewCount is Count),
-    (not(is_empty(Next)) -> solve_s1(Next, InsideRanges, NewCount) ; InsideRanges is NewCount).
-    
-solve_s2([Current|Next], InsideRanges, Count):-
-    string_chars(Ranges, Current),
-    split_string(Ranges, ',', '', Intervals),
-    ((range_overlap(Intervals) -> incr(Count, NewCount)); NewCount is Count),
-    (not(is_empty(Next)) -> solve_s2(Next, InsideRanges, NewCount) ; InsideRanges is NewCount).
+    % Check if one range is inside the other. Increment counter if true.
+    ((range_inside(Intervals) -> incr(CountInside, NewCountInside)); NewCountInside is CountInside),
+    % Check if ranges overlap. Increment counter if true.
+    ((range_overlap(Intervals) -> incr(CountOverlap, NewCountOverlap)); NewCountOverlap is CountOverlap),
+    % Keep going if tail isn't empty...
+    (not(is_empty(Next)) -> solve(Next, InsideRanges, OverlapRanges, NewCountInside, NewCountOverlap)
+    % ...otherwise set output values and return
+    ; (InsideRanges is NewCountInside, OverlapRanges is NewCountOverlap)).
